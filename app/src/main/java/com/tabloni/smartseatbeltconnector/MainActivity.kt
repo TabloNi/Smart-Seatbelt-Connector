dbd3d8cbf33f095810703f14f42236021e9326eb
TabloNi <tabloni0719@gmail.com>
2020-01-21 03:13:42 +0900

    Initial commit

---
+++ app/src/main/java/com/tabloni/smartseatbeltconnector/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.tabloni.smartseatbeltconnector
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
